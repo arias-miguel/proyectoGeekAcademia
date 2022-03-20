e0b9c75d784d34c56d16acf0fcd3abc02eacdcd0
Miguel Arias <arias.miguelm@gmail.com>
2022-03-20 17:25:58 -0500

    creat documents .js

---
+++ proyectoAcademia/modules/showCard.js
@@ -0,0 +1,28 @@
+export const showCards = (data,container)=>{
+
+
+    data.forEach( card => {
+        let {nombre,precio, imagen, descripcion } =card;
+        container.innerHTML += `
+        <article class="card">
+          <div class="header-card">
+            <img src="${imagen}" alt="">
+          </div>
+          <div class="footer-card">
+            <div class="categoria-card">
+              <span>${nombre}</span>
+            </div>  
+              <div class="texto-card">
+                <p>${descripcion}
+                </p>
+                <h3>Precio</h3>
+                <h3>$ ${precio}</h3>
+              </div>
+              <button class="btn" type="submit">comprar
+              </button>
+          </div>
+        </article>
+        `
+    });
+
+}
--- proyectoAcademia/scripts/login.js
+++ proyectoAcademia/scripts/login.js
@@ -1,1 +1,25 @@
-let=0;
+let form = document.getElementById('loginForm');
+
+form.addEventListener('submit',(e)=>{
+
+    e.preventDefault();
+    let emailU = document.getElementById('email').value;
+    let passworU = document.getElementById('password').value;
+
+    fetch('http://localhost:4000/Usuarios',{
+        method: 'POST',
+        body: JSON.stringify({
+            userEmail: emailU,
+            userPassword: passworU
+        }),
+        headers:{
+            'Content-Type': "application/json; charset=UTF-8"
+        }
+    }).then(()=>{
+        window.location.href="./pages/menu.html";
+        alert("informacion guardada correctamente")  
+    }).catch(()=>{
+        alert("error")
+    })
+
+})
